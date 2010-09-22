83127d01fb813d8e5d9946706eedb628b4ff8440
landlessness <brian@landlessness.net>
2010-09-22 04:16:36 -0700

    rails shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrumpleAddons::Application.config.secret_token = '717c8c87c3dc976ce755153efd5b2e8bed4a15a79848b300598db4d36f6809d435a9444b880ede93d22e2dacdf599cd215da9ec458c3563cbedfe95c06652901'
